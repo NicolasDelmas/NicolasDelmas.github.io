4c4229019fb2e6c1d44892f2e95f9b678ee7ae2f
Nicolas Delmas <nide60820@eleve.isep.fr>
2025-02-12 17:59:03 +0100

    galerie_de_peinture

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
             window.location.href = "../Pages/Chateau_de_Chantilly.html";
             break;
 
+        case "galerie de peinture":
+            window.location.href = "../Pages/Galerie_de_Peinture.html";
+            break;
+
         case "grande écuries":
             window.location.href = "../Pages/Chateau_de_Chantilly.html";
             break;
         default:
-            document.getElementById("response").innerHTML = "le lieu  "+ query.toUpperCase() +"  n'existe pas.";
+            document.getElementById("response").innerHTML = "le lieu "+ query.toUpperCase() +" n'existe pas.";
     }
 });
+
+
+function displayImage() {
+    const selectedImage = document.querySelector('input[name="dam"]:checked').value;
+    const imageContainer = document.getElementById('imageContainer');
+
+    imageContainer.innerHTML = '';
+    const img = document.createElement('img');
+    
+    img.src = `../Images/Enigme/1_Nom_du_cheval/${selectedImage}.jpg`; 
+    img.alt = selectedImage;
+    imageContainer.appendChild(img);
+}
